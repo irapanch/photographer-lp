aafa255d2e804de04dab82f9e38d1758f3310e93
Veronica <vveronikanika@gmail.com>
2023-08-08 00:12:29 +0300

    feat: create checkbox menu in timeline section

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
+// import { accordion } from './js/modules/accordion';
 import {saultAnimation} from './js/modules/sault-animation';
 
 document.addEventListener("DOMContentLoaded", () => {
   // console.log(saultAnimation);
   saultAnimation();
 
-  $(".hover").mouseleave(
-    function() {
-      $(this).removeClass("hover");
-    }
-  );
+  // $(".hover").mouseleave(
+  //   function() {
+  //     $(this).removeClass("hover");
+  //   }
+  // );
+
+  // accordion();
 
 });
