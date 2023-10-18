ad8448e3a280e66cfc3227d3f5896f8f9334827b
veronikanos <vveronikanika@gmail.com>
2023-10-18 23:31:25 +0300

    fix: change color palette, slider

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -1,4 +1,4 @@
-import Swiper, { Autoplay, Navigation, Pagination } from 'swiper';
+import Swiper, { Autoplay, EffectCoverflow, Navigation, Pagination } from 'swiper';
 import { Navigation, Pagination } from 'swiper';
 // import Swiper and modules styles
 import 'swiper/swiper-bundle.min.css';
@@ -12,12 +12,33 @@ export const slider = () => {
 const swiper = new Swiper(".swiper", {
   spaceBetween: 30,
   centeredSlides: true,
-  slidesPerView: 3,
+  slidesPerView: 1,
   loop: true,
-  modules: [ Autoplay, Navigation, Pagination ],
+  effect: "coverflow",
+  modules: [ Autoplay, Navigation, Pagination, EffectCoverflow],
   autoplay: {
     delay: 5000,
     // disableOnInteraction: false,
+  },
+  coverflowEffect: {
+      rotate: 0,
+  //     stretch: 80,
+      depth: 200,
+      modifier: 1,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 1.5,
+      spaceBetween: 70,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 80,
+    },
   },
   pagination: {
     el: ".swiper-pagination",
