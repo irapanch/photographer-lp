27784f447a67f0b928eeaa6ea4502e9f3c000c33
veronikanos <vveronikanika@gmail.com>
2023-11-25 19:29:41 +0200

    tab realization in services section

--- src/js/content/slider.js
+++ src/js/content/slider.js
@@ -9,48 +9,56 @@ const womanPortrait = require('../../images/jpg/woman-portrait.jpeg');
 
 export const sliderContent = [
   {
+    id: 1,
     src: business,
     alt: 'Lorem ipsum...',
     title: 'Бізнес зйомка',
     description: 'Lorem ipsum dolor...',
   },
   {
+    id: 2,
     src: content,
     alt: 'Lorem ipsum...',
     title: 'Контентна',
     description: 'Lorem ipsum dolor...',
   },
   {
+    id: 3,
     src: family,
     alt: 'Lorem ipsum...',
     title: 'Cімейна',
     description: 'Lorem ipsum dolor...',
   },
   {
+    id: 4,
     src: forBlog,
     alt: 'Lorem ipsum...',
     title: 'Для блогу',
     description: 'Lorem ipsum dolor...',
   },
   {
+    id: 5,
     src: loveStory,
     alt: 'Lorem ipsum...',
     title: 'Love Story',
     description: 'Lorem ipsum dolor...',
   },
   {
+    id: 6,
     src: sacraments,
     alt: 'Lorem ipsum...',
     title: 'Таїнства',
     description: 'Lorem ipsum dolor...',
   },
   {
+    id: 7,
     src: wedding,
     alt: 'Lorem ipsum...',
     title: 'Весільна',
     description: 'Lorem ipsum dolor...',
   },
   {
+    id: 8,
     src: womanPortrait,
     alt: 'Lorem ipsum...',
     title: 'Портретна',
--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -1,22 +1,33 @@
-import Swiper, { Autoplay, Navigation, Pagination } from 'swiper';
-import { Navigation, Pagination } from 'swiper';
+import Swiper from 'swiper';
+import { Navigation, Pagination, Autoplay } from 'swiper';
 import 'swiper/swiper-bundle.min.css';
 import 'swiper/swiper-bundle.min.js';
 
 import { sliderContent } from '../content/slider';
 
 export const slider = () => {
+  const changeActiveTab = (tabs, id) => {
+    tabs.forEach(item => {
+      item.classList.remove('active');
+      if (Number(item.dataset.id) - 1 === id) {
+        item.classList.add('active');
+      }
+    });
+  };
+
   const swiper = new Swiper('.swiper', {
     spaceBetween: 30,
     centeredSlides: true,
-    slidesPerView: 1,
-    loop: true,
-    mousewheelControl: true,
+    // slidesPerView: 1,
+    // loop: true,
     keyboardControl: true,
     updateOnWindowResize: true,
     slideToClickedSlide: true,
     modules: [Autoplay, Navigation, Pagination],
-    speed: 2000,
+    grabCursor: true,
+    // initialSlide: 1,
+    speed: 5000,
+
     autoplay: {
       delay: 3000,
       disableOnInteraction: false,
@@ -42,13 +53,15 @@ export const slider = () => {
     },
   });
 
+  // const swiper = document.querySelector('.swiper').sliderSwiper;
+
   const swiperWrapper = document.querySelector('.swiper-wrapper');
 
   let slides = '';
 
   sliderContent.forEach(item => {
     slides += `
-    <div class="swiper-slide">
+    <div class="swiper-slide" data-id=${item.id}>
           <div class="services__card">
             <img src=${item.src} alt=${item.alt} />
             <div class="content">
@@ -64,4 +77,37 @@ export const slider = () => {
   });
 
   swiperWrapper.insertAdjacentHTML('beforeend', slides);
+
+  // Handle click to tab and go to clicked title slide
+  const tabs = document.querySelector('.tabs');
+
+  const allTabs = [];
+  sliderContent.forEach(item =>
+    allTabs.push(
+      `<button type="button" class="tabButton" data-id=${item.id}>
+    ${item.title}
+  </button>`
+    )
+  );
+
+  tabs.insertAdjacentHTML('beforeend', allTabs.join(''));
+
+  const tabBtns = document.querySelectorAll('.tabButton');
+
+  tabs.addEventListener('click', e => {
+    e.preventDefault();
+    if (e.target?.tagName != 'BUTTON') return;
+    const clickedTabId = Number(e.target.getAttribute('data-id'));
+    swiper.slideTo(clickedTabId - 1, 1000);
+
+    changeActiveTab(tabBtns, clickedTabId - 1);
+    // console.log(swiper.realIndex);
+  });
+
+  swiper.on('slideChange', e => {
+    const activeSlide = document.querySelector('.swiper-slide-active');
+    const id = Number(activeSlide.getAttribute('data-id'));
+
+    changeActiveTab(tabBtns, id);
+  });
 };
