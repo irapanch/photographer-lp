fe434114dcb6d2768994bcd3c53c2d264cce97f1
veronikanos <vveronikanika@gmail.com>
2023-12-27 23:32:00 +0200

    feat: add baguette library for portfolio gallery

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,23 +1,32 @@
-import { benefits } from './js/modules/benefits';
-import { timeline } from './js/modules/timeline';
-import { accordion } from './js/modules/accordion';
-import { faq } from './js/modules/faq';
-import { photocardsAnimation } from './js/modules/photocardsAnimation';
-import { slider } from './js/modules/slider';
+// import { benefits } from './js/modules/benefits';
+// import { timeline } from './js/modules/timeline';
+// import { accordion } from './js/modules/accordion';
+// import { faq } from './js/modules/faq';
+// import { photocardsAnimation } from './js/modules/photocardsAnimation';
+// import { slider } from './js/modules/slider';
 import { form } from './js/modules/form';
 import { burger } from './js/modules/burger';
 import { modals } from './js/modules/modals';
 import { mask } from './js/modules/mask';
+import baguetteBox from 'baguettebox.js';
+import 'baguettebox.js/dist/baguetteBox.min.css';
 
 document.addEventListener('DOMContentLoaded', () => {
   modals();
   form();
-  benefits();
-  photocardsAnimation();
-  slider();
-  timeline();
-  accordion();
-  faq();
+  // benefits();
+  // photocardsAnimation();
+  // slider();
+  // timeline();
+  // accordion();
+  // faq();
   burger('.nav', '.mobile-menu__button');
   mask('[name="phone"]');
+  console.log(baguetteBox);
+  baguetteBox.run('.gallery', {
+    buttons: true,
+    // animation: 'fadeIn',
+  });
+
+  document.body.style.overflow = 'hidden';
 });
