81505fb83b1fd0f191854cfd3dcdf47d3229779a
veronikanos <vveronikanika@gmail.com>
2024-03-19 19:33:45 +0200

    update slider at home page

--- src/js/content/slider.js
+++ src/js/content/slider.js
@@ -1,11 +1,13 @@
 const business = require('../../images/jpg/business.jpeg');
-const product = require('../../images/jpg/content.jpeg');
+const product = require('../../images/jpg/product.jpeg');
 const family = require('../../images/jpg/family.jpeg');
-const content = require('../../images/jpg/for-blog.jpeg');
-const loveStory = require('../../images/jpg/love-story.jpeg');
+const content = require('../../images/jpg/stylist.jpg');
+const loveStory = require('../../images/jpg/loveStory/loveStory6.jpg');
 const sacraments = require('../../images/jpg/sacraments.jpeg');
 // const wedding = require('../../images/jpg/wedding.jpeg');
-const portrait = require('../../images/jpg/woman-portrait.jpeg');
+// const portrait = require('../../images/jpg/woman-portrait.jpeg');
+const portrait = require('../../images/jpg/portrait.jpeg');
+const video = require('../../images/jpg/video.jpg');
 
 export const categories = [
   'business',
@@ -14,17 +16,18 @@ export const categories = [
   'product',
   'loveStory',
   'sacraments',
+  'video',
 ];
 
 export const sliderContent = [
   {
     id: 1,
-    src: business,
-    name: categories[0],
-    alt: 'Дівчина архітектор креслить схеми креслення',
-    title: 'Бізнес зйомка',
+    src: portrait,
+    name: categories[6],
+    alt: 'Дівчина малює біля вікна',
+    title: 'Портретна',
     description:
-      'Ваша візитна картка у світі виробництва та послуг. Від корпоративних портретів до атмосферних знімків робочого процесу - я зроблю ваш бізнес видимим та привабливим для вашої аудиторії.',
+      'Погляньте на себе по-новому! Фотографії, які скажуть про Вас більше, ніж слова. Ідеально підійдуть для Ваших соціальних мереж, робочого або особистого використання.',
   },
   {
     id: 2,
@@ -37,12 +40,12 @@ export const sliderContent = [
   },
   {
     id: 3,
-    src: family,
-    name: categories[2],
-    alt: 'Жінка, чоловік та дитина сидять на пікніку в лісі',
-    title: 'Cімейна',
+    src: business,
+    name: categories[0],
+    alt: 'Дівчина архітектор креслить схеми креслення',
+    title: 'Бізнес',
     description:
-      'Сімейна фотосесія - це час вашого родинного щастя. Від усмішок до сердечних митей – у знімках я збережу те тепло та любов, які заповнюють ваш дім. ',
+      'Ваша візитна картка у світі виробництва та послуг. Від корпоративних портретів до атмосферних знімків робочого процесу - я зроблю ваш бізнес видимим та привабливим для вашої аудиторії.',
   },
   {
     id: 4,
@@ -64,12 +67,29 @@ export const sliderContent = [
   },
   {
     id: 6,
+    src: family,
+    name: categories[2],
+    alt: 'Жінка, чоловік та дитина сидять на пікніку в лісі',
+    title: 'Cімейна',
+    description:
+      'Сімейна фотосесія - це час вашого родинного щастя. Від усмішок до сердечних митей – у знімках я збережу те тепло та любов, які заповнюють ваш дім. ',
+  },
+  {
+    id: 7,
     src: sacraments,
     name: categories[5],
     alt: 'Пара стоїть в церкві перед вівтарем',
     title: 'Таїнства',
     description:
       'Від хрещення до вінчання – я зафіксую кожну таїнственну подію з відчуттями та святістю, яку вони несуть.',
+  },
+  {
+    id: 8,
+    src: video,
+    name: categories[7],
+    alt: '',
+    title: 'Відеозйомка',
+    description: 'Текст ',
   },
   // {
   //   id: 7,
@@ -79,13 +99,4 @@ export const sliderContent = [
   //   description:
   //     'Довірте мені зафіксувати кожен незабутній момент вашого весілля, щоб ви могли насолоджуватися цими спогадами весь свій шлях разом.',
   // },
-  {
-    id: 7,
-    src: portrait,
-    name: categories[6],
-    alt: 'Дівчина малює біля вікна',
-    title: 'Портретна',
-    description:
-      'Погляньте на себе по-новому! Фотографії, які скажуть про Вас більше, ніж слова. Ідеально підійдуть для Ваших соціальних мереж, робочого або особистого використання.',
-  },
 ];
